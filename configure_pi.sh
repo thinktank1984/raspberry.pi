c03ab7f022d5ff71bf5e0da3633904a1f0876603
Ed Sharood <ed.s.sharood@gmail.com>
2025-03-14 12:51:23 +1300

    .

--- configure_pi.sh
+++ configure_pi.sh
@@ -1,3 +1,5 @@
+
+
 echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions 
 edsharood@192.168.178.100:"D:/sandbox/movies" /mnt/movies
 echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions 
