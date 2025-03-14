3dd0535795623e94dbb24919de8bf61d4d92603d
Ed Sharood <ed.s.sharood@gmail.com>
2025-03-14 14:25:18 +1300

    .

--- configure_pi.sh
+++ configure_pi.sh
@@ -1,8 +1,9 @@
 
 
-echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions 
+echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions \
 edsharood@192.168.178.100:"D:/sandbox/movies" /mnt/movies
-echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions 
-edsharood@192.168.178.100:"D:\docker-folder\complete" /mnt/complete
+echo "As-12345699" | sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions \
+edsharood@192.168.178.100:"D:/docker-folder/complete" /mnt/complete
+
 sudo hostnamectl set-hostname homeserver
 sudo sed -i 's/raspberrypi/homeserver/g' /etc/hosts
--- one_time_config.sh
+++ one_time_config.sh
@@ -1,4 +1,6 @@
+sudo apt-get update
 sudo apt-get install sshfs
+sudo apt-get install sshpass
 chmod +x configure_pi.sh
 sudo mkdir /mnt/movies
 sudo mkdir /mnt/complete
