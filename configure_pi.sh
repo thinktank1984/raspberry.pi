50c13f6d1b4654538ce6223cd172e02d9eebeded
Ed Sharood <ed.s.sharood@gmail.com>
2025-03-14 14:45:07 +1300

    .

--- configure_pi.sh
+++ configure_pi.sh
@@ -1,7 +1,8 @@
+sudo mount -t cifs //192.168.178.100/movies /mnt/movies \
+  -o username=edsharood,password=As-12345699
 
-
-sshpass -p "As-12345699"  sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions edsharood@192.168.178.100:"D:/sandbox/movies" /mnt/movies
-sshpass -p "As-12345699"  sudo sshfs -o password_stdin,idmap=user,allow_other,default_permissions edsharood@192.168.178.100:"D:/docker-folder/complete" /mnt/complete
+sudo mount -t cifs //192.168.178.100/complete /mnt/complete \
+  -o username=edsharood,password=As-12345699
 
 sudo hostnamectl set-hostname homeserver
 sudo sed -i 's/raspberrypi/homeserver/g' /etc/hosts
